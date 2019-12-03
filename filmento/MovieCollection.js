5805daa1ecc63200515d691bb6b2f922c4fdc587
eronchang <45950088+eronchang@users.noreply.github.com>
2019-12-03 17:31:11 -0500

    login, sign up(halfway) done

---
+++ filmento/MovieCollection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export class MovieCollectionPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+      }
+
+
+
+      render() {
+        return (
+            <View>
+                <Text>Test</Text>
+            </View>
+        )
+    }
+}
---
+++ filmento/SignUp.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { View, TextInput, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import Firebase from './Firebase';
+
+
+export class SignUpPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            firstName: '',
+            lastName:'',
+            username:'',
+            email: '',
+            password: ''
+        }
+      }
+
+      handleSignUp = () => {
+        const { email, password } = this.state
+        Firebase.auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(() => this.props.navigation.navigate('MovieCollection'))
+            .catch(error => console.log(error))
+    }
+
+      render() {
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.inputBox}
+                    value={this.state.firstName}
+                    onChangeText={firstName => this.setState({ firstName })}
+                    placeholder='First Name'
+                />
+                <TextInput
+                    style={styles.inputBox}
+                    value={this.state.lastName}
+                    onChangeText={lastName => this.setState({ lastName })}
+                    placeholder='Last Name'
+                />
+                <TextInput
+                    style={styles.inputBox}
+                    value={this.state.username}
+                    onChangeText={username => this.setState({ username })}
+                    placeholder='Create Your Username'
+                />
+                <TextInput
+                    style={styles.inputBox}
+                    value={this.state.email}
+                    onChangeText={email => this.setState({ email })}
+                    placeholder='Email'
+                    autoCapitalize='none'
+                />
+                <TextInput
+                    style={styles.inputBox}
+                    value={this.state.password}
+                    onChangeText={password => this.setState({ password })}
+                    placeholder='Password'
+                    secureTextEntry={true}
+                />
+                <TouchableOpacity style={styles.button} onPress={this.handleSignUp}>
+                    <Text style={styles.buttonText}>Signup</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    inputBox: {
+        width: '85%',
+        margin: 10,
+        padding: 15,
+        fontSize: 16,
+        borderColor: '#d3d3d3',
+        borderBottomWidth: 1,
+        textAlign: 'center'
+    },
+    button: {
+        marginTop: 30,
+        marginBottom: 20,
+        paddingVertical: 5,
+        alignItems: 'center',
+        backgroundColor: '#FFA611',
+        borderColor: '#FFA611',
+        borderWidth: 1,
+        borderRadius: 5,
+        width: 200
+    },
+    buttonText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff'
+    },
+    buttonSignup: {
+        fontSize: 12
+    }
+})
